94faa3de5c433c58e0729b184e1e10dda040c6c9
yusufnafey <yusufnafey@yahoo.com>
2019-11-08 10:09:20 -0800

    finishes mvp

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,8 +97,26 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
-
+        self.set_light_on()
+        while self.light_is_on(): # keep running until sorting done
+            # turn light off every time loop starts
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                # compare, swap, turn light back on
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+            self.swap_item()
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+                # compare, swap, turn light back on
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+            self.swap_item()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
